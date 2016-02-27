015a0429e05af38e96d2a2c5d3d58e4b6825b104
syntactical <greg.dutcher@gmail.com>
2016-02-27 11:06:05 -0500

    get LED working

--- hacklab_demo.ino
+++ hacklab_demo.ino
@@ -1,9 +1,28 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <NewPing.h>
 
+#define TRIGGER_PIN  3
+#define ECHO_PIN     2
+#define LED_PIN 6
+#define MAX_DISTANCE 200
+
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
+
+void setup() {
+  Serial.begin(115200);
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  delay(50);
+  int uS = sonar.ping();
+  
+  int distance = uS / US_ROUNDTRIP_CM;
+  
+  Serial.println(distance);
 
+  if (distance < 20 && distance != 0) {
+    digitalWrite(LED_PIN, HIGH);
+  } else {
+    digitalWrite(LED_PIN, LOW);
+  }
 }
